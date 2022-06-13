8cee5a1142e75bf3c60dbd4fae184b07b2ba5b16
vinchantle <chenvincent610@gmail.com>
2022-06-13 10:40:48 +0800

    latest version (47epochs)

--- shufflecsv.py
+++ shufflecsv.py
@@ -4,14 +4,14 @@ import os
 curpath = os.path.join(os.getcwd(),'train_simplified')
 destpath = os.path.join(os.getcwd(), 'shuffled')
 
-
-for i in range(100):
+df = pd.concat([pd.read_csv(os.path.join(curpath,f), nrows = 20000) for f in os.listdir(curpath)])
+shuffled = df.sample(n=6800000)
+for i in range(340):
     print(i)
-    df = pd.concat([pd.read_csv(os.path.join(curpath,f), nrows = 500, skiprows=[j for j in range(1, 500*i+1)]) for f in os.listdir(curpath)])
-    suffled = df.sample(n=100000)
-    suffled.to_csv(os.path.join(destpath, "train_data_{}.csv".format(i+1)))
+    chosen = shuffled.iloc[i*20000:(i+1)*20000]
+    chosen.to_csv(os.path.join(destpath, "train_data_{}.csv".format(i+1)))
     
 
-validate = pd.concat([pd.read_csv(os.path.join(curpath, f), nrows = 100, skiprows=[i for i in range(1, 50000+1)]) for f in os.listdir(curpath)])
+validate = pd.concat([pd.read_csv(os.path.join(curpath, f), nrows = 100, skiprows=[i for i in range(1, 20000+1)]) for f in os.listdir(curpath)])
 validate_shuffle = validate.sample(n=34000)
 validate_shuffle.to_csv(os.path.join(destpath,"valid_data.csv"))
