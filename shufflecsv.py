55e97f311198c91ecd14d42cf7175a6cb6023a3b
vinchantle <chenvincent610@gmail.com>
2022-06-15 00:31:38 +0800

    updated

--- shufflecsv.py
+++ shufflecsv.py
@@ -4,11 +4,11 @@ import os
 curpath = os.path.join(os.getcwd(),'train_simplified')
 destpath = os.path.join(os.getcwd(), 'shuffled')
 
-df = pd.concat([pd.read_csv(os.path.join(curpath,f), nrows = 20000) for f in os.listdir(curpath)])
-shuffled = df.sample(n=6800000)
-for i in range(340):
+df = pd.concat([pd.read_csv(os.path.join(curpath,f), nrows = 4000) for f in os.listdir(curpath)])
+shuffled = df.sample(n=4000*340)
+for i in range(100):
     print(i)
-    chosen = shuffled.iloc[i*20000:(i+1)*20000]
+    chosen = shuffled.iloc[i*13600:(i+1)*13600]
     chosen.to_csv(os.path.join(destpath, "train_data_{}.csv".format(i+1)))
     
 
